5fa2b5a53b88ba5c58f84fb870638e22ff90187e
VEIC\tbarrett <tbarrett@veic.org>
2018-03-07 12:15:40 -0500

    Upgraded package requirements
    statsmodels, a dependency of eemeter 1.3.3, is added here has an explicit requirement of this package because doing so is currently required to create an eemeter 1.3.3 conda environment on Windows

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ setup(name='thermostat',
     packages=find_packages(),
     package_data={'': ['*.csv']},
     install_requires=[
-        'eemeter==0.4.8',
-        'pandas==0.20.3',
+        'eemeter==1.3.3',
+        'pandas==0.22.0',
+        'statsmodels==0.8.0'
     ],
 )
